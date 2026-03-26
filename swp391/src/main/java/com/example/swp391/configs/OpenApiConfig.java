eff4b9a216e2883fd099098947ce2da0c1112e90
Lag500ms <vinhly123tn61@gmail.com>
2026-03-27 01:22:01 +0700

    fix

--- swp391/src/main/java/com/example/swp391/configs/OpenApiConfig.java
+++ swp391/src/main/java/com/example/swp391/configs/OpenApiConfig.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,21 +17,23 @@ public class OpenApiConfig {
     public OpenAPI openAPI(@Value("${application-title:User Service API}") String title,
                            @Value("${application-description:API for user management}") String description,
                            @Value("${application-version:1.0.0}") String version,
-                           @Value("${application-license:Apache 2.0}") String license) {
-        return new OpenAPI().addSecurityItem(new SecurityRequirement()
-                .addList("Bearer Authentication"))
-            .components(new Components().addSecuritySchemes
-                ("Bearer Authentication", createAPIKeyScheme()))
-            .info(new Info()
-                .title(title)
-                .description(description)
-                .version(version)
-                .license(new License().name(license)));
+                           @Value("${application-license:Apache 2.0}") String license,
+                           @Value("${application-server-url:https://swp391sp26backend-production.up.railway.app/}") String serverUrl) { // thêm serverUrl
+        return new OpenAPI()
+                .addServersItem(new Server().url(serverUrl)) // thêm server
+                .addSecurityItem(new SecurityRequirement()
+                        .addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info()
+                        .title(title)
+                        .description(description)
+                        .version(version)
+                        .license(new License().name(license)));
     }
 
     private SecurityScheme createAPIKeyScheme() {
         return new SecurityScheme().type(SecurityScheme.Type.HTTP)
-            .bearerFormat("JWT")
-            .scheme("bearer");
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
